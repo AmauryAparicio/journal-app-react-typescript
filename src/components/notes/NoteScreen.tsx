df7a00704ee39ceb7d8a3d1b2793c2437f3d9768
Amaury <amaurysunstar@gmail.com>
2020-10-22 17:00:47 -0500

    Styles finished

--- src/components/notes/NoteScreen.tsx
+++ src/components/notes/NoteScreen.tsx
@@ -5,6 +5,26 @@ const NoteScreen: FunctionComponent = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          name=""
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          name=""
+          placeholder="What happened today?"
+          className="notes__textarea"
+        ></textarea>
+        <div className="notes__image">
+          <img
+            src="https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero.jpg"
+            alt="img"
+          />
+        </div>
+      </div>
     </div>
   );
 };
